be6af30601be526b6e8c14f74be487e6bb6a49e8
Chishan <103311714+ChishanB@users.noreply.github.com>
2025-10-02 10:56:50 +1000

    Distribution plots and volcano plots
    Utilising multimiR results to examine miRNA gene targeting

---
+++ distribution_plot_July.R
@@ -0,0 +1,284 @@
+# Date: 28/7/25
+# Author: Chishan Burch
+# Purpose: 
+# (Instructions from John): Single omics analysis – link mature miRNA to mRNA targets 
+
+#Generate a dataframe which shows us the common miRNA targets. What this means is that if we had 10 mature miRNA that were DE in the dataset, how many of these target the same gene. What I envision is that we would have a list of genes from multimiR data, and for each of those genes, you would have all the miRNA from the DE miRNA that target it – ie Gene 1 might be targeted by miR-223, miR-155 and miR-146a, while gene 2 might only be targeted by miR-223
+#Plot this as a distribution plot showing the number of genes on y axis, and number of miRNA that target each gene on x. So you might have 300 genes that are targeted by only one miRNA, but 30 genes targeted by 2 and so on
+#The final step will be to link the DE miRNA with appropriately regulated DE genes. So remember, if a gene is a real target of a down-regulated miRNA, you would expect it to be up-regulated and what we are hoping you will do is plot on a volcano plot all gene targets of the down-regulated miRNA. So the volcano plot will only show those gene targets and it will see how many of those are up-regulated. You will then repeat this with the up-regulated miRNA and down-regulated targets. 
+
+
+library(openxlsx)
+library(dplyr)
+library(tibble)
+library(DESeq2)
+library(ggplot2)
+library(openxlsx)
+library(readxl)
+library(ggrepel)
+library(edgeR)
+library(data.table)
+
+
+miRNAs <- fread(file = "./3_results/july_DESeq_sncRNAs.csv")
+
+
+miRNAs <- miRNAs %>%
+  filter(sign_DE != "NS")
+
+
+miRNAs <- miRNAs %>%
+  filter(grepl("mature", V1))
+
+miRNAs$V1 <- gsub(" mature$", "", miRNAs$V1)
+
+
+# miRNAs contains all differentially expressed miRNAs. 
+# We want to know how many target the same genes.
+
+miRNA_targets_up <- fread(file = "./3_results/multimiR_mature_mirnas_up_validated.csv")
+miRNA_targets_down <- fread(file = "./3_results/multimiR_mature_mirnas_down_validated.csv")
+
+miRNA_targets <- rbind(miRNA_targets_up,miRNA_targets_down)
+
+#> intersect(miRNAs$V1, miRNA_targets$mature_mirna_id)
+#[1] "mmu-miR-128-3p"  "mmu-miR-145a-5p" "mmu-miR-199a-3p" "mmu-miR-199b-3p" "mmu-miR-19b-3p"  "mmu-miR-211-5p"  "mmu-miR-221-3p" 
+#[8] "mmu-miR-410-3p"  "mmu-miR-532-5p" 
+
+# So 8 of the DE miRNAs had targets in multimiR
+
+common_ids <- intersect(miRNAs$V1, miRNA_targets$mature_mirna_id)
+
+filtered_targets <- miRNA_targets %>%
+  filter(mature_mirna_id %in% common_ids)
+
+nrow(filtered_targets)
+miRNA_target_numbers <- table(filtered_targets$mature_mirna_id)
+
+#write.csv(miRNA_target_numbers, file = "./3_results/july_DE_mature_miRNA_hits_freq.csv")
+# > 2231 + 441 + 156 + 149 + 7263 + 175 + 4949 + 473 + 630
+#[1] 16467
+
+
+# Summarize: for each gene, list all DE miRNAs that target it
+gene_miRNA_df <- filtered_targets %>%
+  group_by(target_symbol) %>%
+  summarise(
+    miRNAs = paste(unique(mature_mirna_id), collapse = ", "),
+    n_miRNAs = n_distinct(mature_mirna_id)
+  ) %>%
+  arrange(desc(n_miRNAs))  # optional: sort by number of miRNAs
+
+
+#write.csv(gene_miRNA_df, file = "./3_results/miRNA_targets_september.csv")
+
+
+barplot <- ggplot(gene_miRNA_df, aes(x = n_miRNAs)) +
+  geom_bar(fill = "#0073C2FF") +
+  labs(
+    title = "Number of DE miRNAs Targeting Each Gene detected in the mouse SV",
+    x = "Number of DE miRNAs targeting the same gene",
+    y = "Number of genes"
+  ) +
+  theme_minimal()
+
+plot(barplot)
+#ggsave(
+#  filename = "./2_figures/miRNA_distribution_plot.png", # change to whatever you like
+ # plot     = barplot,
+#  width    = 8,    # in inches
+  #height   = 6,    # in inches
+ # dpi      = 300   # resolution
+#)
+
+
+# For each group of DE miRNAs (upregulated or downregulated), look at their gene 
+# targets, then check whether those genes are appropriately regulated in your DE mRNA dataset.
+
+DE_mRNAs <- fread("./3_results/april_DESeq_genes.csv")
+
+
+miRNAs <- miRNAs %>%
+  mutate(sign_DE = if_else(padj < 0.05 & log2FoldChange < 0, "Downregulated",
+                           if_else(padj < 0.05 & log2FoldChange > 0, "Upregulated", "NS")))
+
+DE_mRNAs <- DE_mRNAs %>%
+  mutate(sign_DE = if_else(padj < 0.05 & log2FoldChange < 0, "Downregulated",
+                           if_else(padj < 0.05 & log2FoldChange > 0, "Upregulated", "NS")))
+# Downregulated miRNAs (should release repression, allowing targets to go UP)
+down_miRNAs <- miRNAs %>%
+  filter(sign_DE == "Downregulated") %>%
+  pull(V1)
+
+# Upregulated miRNAs (should enhance repression, so targets go DOWN)
+up_miRNAs <- miRNAs %>%
+  filter(sign_DE == "Upregulated") %>%
+  pull(V1)
+
+
+# Get target genes of each miRNA group
+down_targets <- filtered_targets %>%
+  filter(mature_mirna_id %in% down_miRNAs) %>%
+  distinct(target_ensembl)
+
+up_targets <- filtered_targets %>%
+  filter(mature_mirna_id %in% up_miRNAs) %>%
+  distinct(target_ensembl)
+
+# Genes targeted by downregulated miRNAs
+down_miRNA_gene_targets <- DE_mRNAs %>%
+  filter(V1 %in% down_targets$target_ensembl)
+
+# Genes targeted by upregulated miRNAs
+up_miRNA_gene_targets <- DE_mRNAs %>%
+  filter(V1 %in% up_targets$target_ensembl)
+
+colours_1 <- c("Sign_Up" = "#994455", "Sign_Down" = "#0077BB", "NS" = "#BBBBBB") 
+
+opacity <- c("Sign_Up" = 1, "Sign_Down" = 1, "NS" = 1)
+
+
+ggplot(down_miRNA_gene_targets, aes(x = log2FoldChange, y = -log10(padj))) +
+  geom_point(aes(color = log2FoldChange > 0), alpha = 1) +
+  scale_color_manual(values = c("TRUE" = "#994455", "FALSE" = "grey")) +
+  labs(title = "Targets of Downregulated miRNAs (Expected ↑)",
+       x = "log2 Fold Change (mRNA)",
+       y = "-log10 adjusted p-value") +
+  theme_minimal()
+
+ggplot(up_miRNA_gene_targets, aes(x = log2FoldChange, y = -log10(padj))) +
+  geom_point(aes(color = log2FoldChange < 0), alpha = 1) +
+  scale_color_manual(values = c("TRUE" = "#0077BB", "FALSE" = "grey")) +
+  labs(title = "Targets of Upregulated miRNAs (Expected ↓)",
+       x = "log2 Fold Change (mRNA)",
+       y = "-log10 adjusted p-value") +
+  theme_minimal()
+
+
+
+plot_miRNA_targets <- up_miRNA_gene_targets %>%
+  filter(!is.na(sign_DE)) %>%
+  ggplot(aes(x = log2FoldChange, y = -log10(padj), color = sign_DE, fill = sign_DE, alpha = sign_DE)) +
+  geom_point(size = 5, shape = 21, color = "black") +
+  geom_hline(yintercept = -log10(0.05), linetype = "dashed", size = 0.4, col = "#3A3B3C") +
+  scale_color_manual(values = colours_1) +
+  scale_fill_manual(values = colours_1) +
+  scale_alpha_manual(values = opacity) +
+  labs(
+    title = "Targets of Upregulated miRNAs (Expected ↓)",
+    x = "Log2 Fold Change (mRNA)",
+    y = "-Log10 adjusted p-value",
+    color = "Effect in treated group",
+    fill = "Effect in treated group",
+    alpha = "Effect in treated group"
+  ) +
+  theme_bw() +
+  theme(
+    axis.text = element_text(size = 14),
+    axis.title = element_text(size = 18),
+    legend.text = element_text(size = 14),
+    legend.title = element_text(size = 14)
+  )
+
+plot_miRNA_targets2 <- down_miRNA_gene_targets %>%
+  filter(!is.na(sign_DE)) %>%
+  ggplot(aes(x = log2FoldChange, y = -log10(padj), color = sign_DE, fill = sign_DE, alpha = sign_DE)) +
+  geom_point(size = 5, shape = 21, color = "black") +
+  geom_hline(yintercept = -log10(0.05), linetype = "dashed", size = 0.4, col = "#3A3B3C") +
+  scale_color_manual(values = colours_1) +
+  scale_fill_manual(values = colours_1) +
+  scale_alpha_manual(values = opacity) +
+  labs(
+    title = "Targets of Upregulated miRNAs (Expected ↓)",
+    x = "Log2 Fold Change (mRNA)",
+    y = "-Log10 adjusted p-value",
+    color = "Effect in treated group",
+    fill = "Effect in treated group",
+    alpha = "Effect in treated group"
+  ) +
+  theme_bw() +
+  theme(
+    axis.text = element_text(size = 14),
+    axis.title = element_text(size = 18),
+    legend.text = element_text(size = 14),
+    legend.title = element_text(size = 14)
+  )
+
+plot(plot_miRNA_targets2)
+
+###########
+
+
+genesandTEs <- fread(file = "./3_results/april_DESeq_genesandTEs.csv")
+
+
+genesandTEs <- genesandTEs %>%
+  filter(padj < 0.05)
+
+
+genesandTEs <- genesandTEs %>%
+  filter(grepl("ENSMUS", ensembl.TE))
+
+
+miRNA_targets_up <- fread(file = "./3_results/multimiR_mature_mirnas_up_validated.csv")
+miRNA_targets_down <- fread(file = "./3_results/multimiR_mature_mirnas_down_validated.csv")
+
+miRNA_targets <- rbind(miRNA_targets_up,miRNA_targets_down)
+
+intersect(genesandTEs$hgnc.TE, miRNA_targets$target_symbol)
+#[1] "Col1a1"    "Capns1"    "Ssr4"      "Kmt2a"     "Arid1a"    "Piezo1"    "Psmb6"     "Chd3"      "Dusp6"     "Timp3"     "Ccng1"     "Rack1"     "Sec14l1"  
+#[14] "Nme2"      "Pfkp"      "Hivep1"    "Tkt"       "Bcl6"      "Adcy6"     "Chd1"      "Svil"      "Ehbp1l1"   "Sorbs1"    "Gaa"       "Itih5"     "Igfbp5"   
+#[27] "Pam"       "Atp2b4"    "Col5a1"    "Abca2"     "Fbn1"      "Sord"      "Tpx2"      "Sec62"     "Uox"       "Rps20"     "Coro2a"    "Plin2"     "Rps6"     
+#[40] "Hspg2"     "Map7d1"    "Epb41"     "Mmp17"     "Ncor2"     "Polr1d"    "Col1a2"    "Bhlhe40"   "Cand2"     "Pianp"     "Msn"       "Flna"      "Rab11fip1"
+#[53] "Col4a1"    "Col4a2"    "Cyb5b"     "Nxpe2"     "Map4"      "Arhgef17"  "Prrg3"     "Jcad"      "Ppp1r18"   "Rps27l"    "Egr1"      "Rps21"     "Prune2"   
+#[66] "Srrm2"     "Lrp1"      "Spen"      "Fmod"      "Pgm5"      "Selenok"   "Snai1"     "Rps2"      "Megf8"     "S1pr1"     "Rpl18a"    "Wnk1"      "Camk2n1"  
+#[79] "Kmt2d"     "F2r"       "Vamp8"     "Klf13"     "Tmco1"     "Sec61b"    "Smagp"     "Capn12"    "Tead1"     "Tns1"      "Palld"     "Rpl5"      "Rpl18"    
+#[92] "Kif13b"    "Tpt1"      "H2-K1"     "Fbln2"     "Slfn9"     "Atxn1l"    "Fryl"      "C4b"       "Ppp1r12b"  "Heg1"      "Plekhn1"   "Zfp970"    "Lrrc32"   
+#[105] "Ccl21a"   
+
+
+common_ids <- intersect(genesandTEs$hgnc.TE, miRNA_targets$target_symbol)
+
+filtered_targets <- miRNA_targets %>%
+  filter(target_symbol %in% common_ids)
+
+nrow(filtered_targets)
+miRNA_target_numbers <- table(filtered_targets$mature_mirna_id)
+
+#write.csv(miRNA_target_numbers, file = "./3_results/sep_DE_mature_miRNA_hits_DE_genes_freq.csv")
+
+miRNA_target_numbers <- as.data.frame(miRNA_target_numbers)
+head(miRNA_target_numbers)
+
+
+
+gene_miRNA_df <- filtered_targets %>%
+  group_by(target_symbol) %>%
+  summarise(
+    miRNAs = paste(unique(mature_mirna_id), collapse = ", "),
+    n_miRNAs = n_distinct(mature_mirna_id)
+  ) %>%
+  arrange(desc(n_miRNAs))  # optional: sort by number of miRNAs
+
+
+
+
+barplot <- ggplot(gene_miRNA_df, aes(x = n_miRNAs)) +
+  geom_bar(fill = "#994455") +
+  labs(
+    title = "Number of DE miRNAs Targeting Each DE Gene in the mouse SV",
+    x = "Number of DE miRNAs targeting the same DE gene",
+    y = "Number of genes"
+  ) +
+  theme_minimal()
+
+plot(barplot)
+#ggsave(
+#  filename = "./2_figures/miRNA_distribution_plot_DEgenes.png", 
+#  plot     = barplot,
+#  width    = 8,    # in inches
+#  height   = 6,    # in inches
+#  dpi      = 300   # resolution
+#)
+
+
