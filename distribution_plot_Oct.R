734af33315b58bd681aa742275572e6a3d6ab27d
Chishan <103311714+ChishanB@users.noreply.github.com>
2025-10-30 16:11:24 +1100

    Update distribution_plot_Oct.R
    fixed to work with filtered multimiR results

--- distribution_plot_Oct.R
+++ distribution_plot_Oct.R
@@ -1,4 +1,4 @@
-# Date: 23/10/25
+# Date: 30/10/25
 # Author: Chishan Burch
 # Purpose: 
 
@@ -13,8 +13,9 @@
 #So I regenerated them and changed the colours to be different to the ones 
 #I used to indicate up and downregulation. 
 
-
+# Oct 30 update: Fixed to work with filtered multimir results
 # (Instructions from John): Single omics analysis – link mature miRNA to mRNA targets 
+
 
 
 library(openxlsx)
@@ -27,6 +28,7 @@ library(readxl)
 library(ggrepel)
 library(edgeR)
 library(data.table)
+library(tidyr)
 
 
 miRNAs <- fread(file = "./3_results/july_DESeq_sncRNAs.csv")
@@ -42,10 +44,10 @@ mature <- miRNAs %>%
 mature$V1 <- gsub(" mature$", "", mature$V1)
 
 
-hairpins <- miRNAs %>%
-  filter(grepl("hairpin", V1))
+#hairpins <- miRNAs %>%
+#  filter(grepl("hairpin", V1))
 
-hairpins$V1 <- gsub(" hairpin$", "", hairpins$V1)
+#hairpins$V1 <- gsub(" hairpin$", "", hairpins$V1)
 
 #miRNAs <- rbind(mature,hairpins)
 
@@ -55,33 +57,63 @@ hairpins$V1 <- gsub(" hairpin$", "", hairpins$V1)
 
 # We want to know how many target the same genes.
 
-mature_up <- fread(file = "./3_results/multimiR_mature_mirnas_up_validated.csv")
-mature_down <- fread(file = "./3_results/multimiR_mature_mirnas_down_validated.csv")
-#hairpins_up <- fread(file = "./3_results/multimiR_hairpin_mirnas_up_validated.csv")
-#hairpins_down <- fread(file = "./3_results/multimiR_hairpin_mirnas_down_validated.csv")
+miRNA_targets <- read.csv("./3_results/oct_multimiR_all_targets_of_DE_mature_miRNA.csv")
 
-# Decide whether u want hairpins included
-#miRNA_targets <- rbind(mature_up,mature_down,hairpins_up,hairpins_down)
-miRNA_targets <- rbind(mature_up,mature_down)
-miRNA_targets <- miRNA_targets[ ,!"V1"]
+miRNA_targets <- miRNA_targets[ ,-1]
 
 DE_mature_miRNA_multimiR_results <- fread(file = "./3_results/oct_multimiR_DE_targets_of_DE_mature_miRNA.csv")
-DE_mature_miRNA_multimiR_results <- DE_mature_miRNA_multimiR_results[ ,!"V1"]
+DE_mature_miRNA_multimiR_results <- DE_mature_miRNA_multimiR_results[ ,!1:2]
+
+
+
 
 print(miRNA_targets) # for general distribution plot
 print(DE_mature_miRNA_multimiR_results) # for DE miRNA x DE mRNA distribution plot
 
-############## General distribution plot
+
 
 # Remove duplicates
 miRNA_targets_unique <- miRNA_targets %>%
-  distinct(mature_mirna_id, target_symbol)
+  drop_na(mature_mirna_id, target_symbol) %>%        # remove incomplete rows for key columns
+  distinct(mature_mirna_id, target_symbol, .keep_all = TRUE)  # keep one row per unique pair
+
+
+
+# Reomve TEs, do basic filtering, remove non DE genes
+#DE_genes <- fread(file = "./3_results/julyrerun_DESeq_genesandTEs.csv")
+#DE_genes <- DE_genes %>%
+#  filter(!is.na(padj) & padj < 0.05)
+
+#DE_genes <- DE_genes %>%
+#  filter(grepl("^ENSMUS", V1))
+
+# All genes
+Genes <- fread(file = "./3_results/julyrerun_DESeq_genesandTEs.csv")
+Genes <- Genes %>%
+  drop_na()
+
+Genes <- Genes %>%
+  filter(grepl("^ENSMUS", V1))
+
+colnames(Genes)[colnames(Genes) == "V1"] <- "target_ensembl"
+head(Genes)
+
+
+miRNA_targets_filtered <- miRNA_targets_unique %>%
+  filter(target_ensembl %in% Genes$target_ensembl)
+
+#write.csv(miRNA_targets_filtered, file = "./3_results/oct_multimiR_all_targets_of_DE_mature_miRNA.csv")
+
+
+
+
+############## General distribution plot
 
 # Count unique targets per miRNA
-miRNA_target_counts <- miRNA_targets_unique %>%
+miRNA_target_counts <- miRNA_targets_filtered %>%
   group_by(mature_mirna_id) %>%
   summarise(n_unique_targets = n()) %>%
-  arrange(n_unique_targets)  # optional: sort by target count
+  arrange(n_unique_targets)  # optional: sort by target count 
 
 # Horizontal bar plot
 p1 <- ggplot(miRNA_target_counts, aes(x = reorder(mature_mirna_id, n_unique_targets), y = n_unique_targets)) +
@@ -94,6 +126,7 @@ p1 <- ggplot(miRNA_target_counts, aes(x = reorder(mature_mirna_id, n_unique_targ
   ) +
   theme_minimal()
 
+plot(p1)
 #ggsave(
 #  filename = "./2_figures/miRNA_distribution_plot_matureDEmiRNAs_x_allmRNAs.png", 
 #plot     = p1,
@@ -106,8 +139,18 @@ p1 <- ggplot(miRNA_target_counts, aes(x = reorder(mature_mirna_id, n_unique_targ
 ###################### DE miRNAs linking to DE mRNAs
 
 # Remove duplicates
+#DE_mature_miRNA_multimiR_results_unique <- DE_mature_miRNA_multimiR_results %>%
+#  distinct(mature_mirna_id, target_symbol)
+
+
+# Remove duplicates
 DE_mature_miRNA_multimiR_results_unique <- DE_mature_miRNA_multimiR_results %>%
-  distinct(mature_mirna_id, target_symbol)
+  drop_na(mature_mirna_id, target_symbol) %>%        # remove incomplete rows for key columns
+  distinct(mature_mirna_id, target_symbol, .keep_all = TRUE)  # keep one row per unique pair
+
+
+
+
 
 # Count unique targets per miRNA
 DE_mature_miRNA_multimiR_results_unique_counts <- DE_mature_miRNA_multimiR_results_unique %>%
@@ -126,9 +169,15 @@ p2 <- ggplot(DE_mature_miRNA_multimiR_results_unique_counts, aes(x = reorder(mat
   ) +
   theme_minimal()
 
+plot(p2)
 #ggsave(
-#  filename = "./2_figures/miRNA_distribution_plot_matureDEmiRNAs_x_DEmRNAs.png", 
+# filename = "./2_figures/miRNA_distribution_plot_matureDEmiRNAs_x_DEmRNAs.png", 
 #  width    = 8,    # in inches
 #height   = 6,    # in inches
- #dpi      = 300   # resolution
+#dpi      = 300   # resolution
 #)
+
+
+## Need to re-generate hits frequency table
+intersect(DE_mature_miRNA_multimiR_results$mature_mirna_id, miRNA_targets$mature_mirna_id)
+
